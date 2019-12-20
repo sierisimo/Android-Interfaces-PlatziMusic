2edb36dd60cfb6b0a17734e1eb2919d6d3d55e1d
Sinuhe Jaime <sierisimo@gmail.com>
2019-12-20 17:37:47 -0500

    Temas y estilos: Diferencias

--- app/src/main/java/com/platzi/platzimusic/MainActivity.kt
+++ app/src/main/java/com/platzi/platzimusic/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_main)
 
     }
 }
