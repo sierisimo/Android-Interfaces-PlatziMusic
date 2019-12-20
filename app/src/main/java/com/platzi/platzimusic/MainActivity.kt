7dc9ed27dbced88492f007eebc6d7135f39d603a
Sinuhe Jaime <sierisimo@gmail.com>
2019-12-20 15:58:08 -0500

    Commit inicial

---
+++ app/src/main/java/com/platzi/platzimusic/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.platzi.platzimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_profile)
+
+    }
+}
