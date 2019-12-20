618665fd50b50e3b928ba8152c78393402152c12
Sinuhe Jaime <sierisimo@gmail.com>
2019-12-20 17:08:05 -0500

    ConstraintLayout ejemplificado

---
+++ app/src/main/java/com/platzi/platzimusic/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.platzi.platzimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
